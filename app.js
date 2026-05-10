ab9762e6620a0fe31385844ebca7534fee4feb70
Reggae Martins <skintryker@gmail.com>
2026-05-10 19:48:16 -0400

    Separate hero copy from ark image

--- app.js
+++ app.js
@@ -667,7 +667,7 @@ const screenEyebrow = document.querySelector("#screenEyebrow");
 const backButton = document.querySelector("#backButton");
 const navItems = [...document.querySelectorAll(".nav-item")];
 const soundButton = document.querySelector("#soundButton");
-const assetVersion = "35";
+const assetVersion = "36";
 const serviceWorkerPath = `./sw.js?v=${assetVersion}`;
 const trialStorageKey = "arcakidsTrialUntil";
 const membershipStorageKey = "arcakidsMembershipActive";
--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "arcakids-v35";
+const CACHE_NAME = "arcakids-v36";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=35",
-  "./app.js?v=35",
+  "./styles.css?v=36",
+  "./app.js?v=36",
   "./manifest.webmanifest",
   "./assets/app-icon.svg",
   "./assets/arcakids-logo-transparent.png",
